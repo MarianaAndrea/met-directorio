0fdf8034c862da0a56eca3a8163e277bd3ad7948
fer22nav <fer22navarrete@gmail.com>
2019-09-26 08:51:48 -0300

    correciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { Routes, RouterModule } from '@angular/router';
-import { PerfilComponent } from './components/perfil/perfil.component';
 import { NgModule } from '@angular/core';
+
+import { PerfilComponent } from './components/perfil/perfil.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
 import { BusquedaComponent } from './components/busqueda/busqueda.component';
 import { LoginComponent } from './components/login/login.component'
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'busqueda', component: BusquedaComponent, pathMatch: 'full' },
-  { path: 'perfil', component: PerfilComponent, pathMatch: 'full'},
-  { path: 'formulario', component: FormularioComponent, pathMatch: 'full' },
+  { path: 'busqueda', component: BusquedaComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'formulario', component: FormularioComponent },
 
 ];
 
